2861f7765ef45f0a8e0b4b2d3d2700aaca405ab5
Yayang Ardiansyah <39462527+Iyankz@users.noreply.github.com>
2026-01-28 15:28:40 +0700

    Delete cli directory

--- cli/leuitlogctl.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-from core.storage import Storage
-
-db = Storage("/var/lib/leuitlog/leuitlog.db")
-
-print("Logs:", db.count("logs"))
-print("Devices:", db.count("devices"))
-print("Alerts:", db.count("alerts"))
